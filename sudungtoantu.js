2a69921cbdf1a47e4e0d296e4284f061b88c131c
tarragonser <you@example.com>
2018-12-13 15:01:20 +0700

    summit assignment

---
+++ sudungtoantu.js
@@ -0,0 +1,9 @@
+var width;
+var height;
+
+var width = parseInt(prompt("Enter the width"));
+var height = parseInt(prompt("Enter the Height"));
+
+var area = width * height
+
+document.write("The area is: "+ area)
